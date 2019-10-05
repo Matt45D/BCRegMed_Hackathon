973e0d972c1c1022d976eb3825e5ec846455095c
Matt Davison <matt45davison@gmail.com>
2019-10-04 19:13:32 -0700

    Set up source file for Hello World

---
+++ source.py
@@ -0,0 +1,13 @@
+import json
+import logging
+import os
+from queue import Queue
+from threading import Thread
+from time import time
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
