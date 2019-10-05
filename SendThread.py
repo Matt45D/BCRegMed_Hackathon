bca8e504dbd2ba2d6ee1b63a5dca8a4192b7c51e
Matt Davison <matt45davison@gmail.com>
2019-10-04 20:54:59 -0700

    added in multithreading libraries and created 2 child threads

---
+++ SendThread.py
@@ -0,0 +1,12 @@
+import json
+import logging
+import os
+from queue import Queue
+from threading import Thread
+from time import time
+
+def SendThread():
+    print("Initializing the Sender Thread...")
+
+if __name__ == '__main__':
+    main()
--- source.py
+++ source.py
@@ -1,13 +1,29 @@
 import json
 import logging
 import os
+import threading
+import time
+import sys
+from RecThread import ReceiveThread
+from SendThread import SendThread
 from queue import Queue
 from threading import Thread
 from time import time
 
-def main():
-    print("Hello World!")
 
+def main():
+    print("Hello World From Main Thread!")
+
+    '''Initialize the receiving child thread'''
+    thread1 = threading.Thread(target = ReceiveThread(), args = 10)
+    thread1.start()
+
+    '''Initialize the sending child thread'''
+    thread2 = threading.Thread(target = SendThread(), args = 10)
+    thread2.start()
+
+class MyDrone:
+    
 
 if __name__ == '__main__':
     main()
